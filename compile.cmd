633c733472057bbd7af10bd5f00eb539eec50750
J1M <psgroopic@gmail.com>
2010-10-25 19:39:53 +0200

    Init Commit

--- compile.cmd
+++ compile.cmd
@@ -1,15 +1,15 @@
-del *.hex	/f /q/s
+cd C:\PSGrooPIC\
+del *.hex /f /q /s
 
-ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW301="true" main.c
-ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW310="true" main.c
-ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW315="true" main.c
-ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW341="true" main.c
-ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW301="true" +GWBOOTLOADER="true" main.c
-ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW310="true" +GWBOOTLOADER="true" main.c
-ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW315="true" +GWBOOTLOADER="true" main.c
-ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW341="true" +GWBOOTLOADER="true" main.c
+cd C:\"Program Files (x86)"\PICC
+ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW301="true" C:\PSGrooPIC\main.c
+ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW310="true" C:\PSGrooPIC\main.c
+ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW315="true" C:\PSGrooPIC\main.c
+ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW341="true" C:\PSGrooPIC\main.c
+ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW301="true" +GWBOOTLOADER="true" C:\PSGrooPIC\main.c
+ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW310="true" +GWBOOTLOADER="true" C:\PSGrooPIC\main.c
+ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW315="true" +GWBOOTLOADER="true" C:\PSGrooPIC\main.c
+ccsc +FH +Y9 -L -A -E -M -P -J -D +GFW341="true" +GWBOOTLOADER="true" C:\PSGrooPIC\main.c
 
-#del *.err	/f /q /s
-del *.esym	/f /q /s
-
-pause
+del *.err /f /q /s
+del *.esym /f /q /s
